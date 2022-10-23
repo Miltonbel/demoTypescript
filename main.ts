0e9a7c58c6c66334ba93636a08dbc87b8b1e04b6
Miltonbel <mj.beltran37@uniandes.edu.co>
2022-10-23 17:30:26 +0000

    Add files via upload
    Modificar dinamicamente cambios en html

--- main.ts
+++ main.ts
@@ -1,3 +1,16 @@
 import {Aprendiz, NivelEducativo} from './aprendiz.js';
-export const aprendiz = new Aprendiz("Milton","Beltran", "avatar.png", 30, NivelEducativo.POSGRADO );
+export const aprendiz = new Aprendiz("Milton","Beltran", "avatar.jpg", 30, NivelEducativo.POSGRADO );
 console.log(aprendiz)
+
+let aprendizTable: HTMLElement = document.getElementById("aprendiz")!;
+mostrarDatosApendiz(aprendiz)
+function mostrarDatosApendiz(aprendiz: Aprendiz): void{
+    let tBodyAprendiz = document.createElement("tbody");
+    tBodyAprendiz.innerHTML = ` <tr><td colspan=2> <img src="./${aprendiz.avatar}" height = "100"></tr></td>
+    <tr><td>Nombres:</td><td>${aprendiz.nombres}</td></tr>
+    <tr><td>Apellidos:</td><td>${aprendiz.apellidos}</td></tr>
+    <tr><td>Nivel educativo:</td><td>${aprendiz.nivelEducatiovo}</td></tr>
+    <tr><td>Edad:</td><td>${aprendiz.edad}</td></tr>`
+
+    aprendizTable.appendChild(tBodyAprendiz);
+}
--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,10 @@
 import { Aprendiz, NivelEducativo } from './aprendiz.js';
-export var aprendiz = new Aprendiz("Milton", "Beltran", "avatar.png", 30, NivelEducativo.POSGRADO);
+export var aprendiz = new Aprendiz("Milton", "Beltran", "avatar.jpg", 30, NivelEducativo.POSGRADO);
 console.log(aprendiz);
+var aprendizTable = document.getElementById("aprendiz");
+mostrarDatosApendiz(aprendiz);
+function mostrarDatosApendiz(aprendiz) {
+    var tBodyAprendiz = document.createElement("tbody");
+    tBodyAprendiz.innerHTML = " <tr><td colspan=2> <img src=\"./" + aprendiz.avatar + "\" height = \"100\"></tr></td>\n    <tr><td>Nombres:</td><td>" + aprendiz.nombres + "</td></tr>\n    <tr><td>Apellidos:</td><td>" + aprendiz.apellidos + "</td></tr>\n    <tr><td>Nivel educativo:</td><td>" + aprendiz.nivelEducatiovo + "</td></tr>\n    <tr><td>Edad:</td><td>" + aprendiz.edad + "</td></tr>";
+    aprendizTable.appendChild(tBodyAprendiz);
+}
